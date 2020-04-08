8b7c2486bc735135f016b6595429c9ba2a151379
Janice Smith <janice.k.n.smith@gmail.com>
2020-04-08 16:20:50 -0400

    add the tests for section b

--- spec/section_b_adapter_pattern_spec.rb
+++ spec/section_b_adapter_pattern_spec.rb
@@ -17,22 +17,38 @@ describe SectionBAdapterPattern do
   end
 
   it "has an adapter which functions" do
-    # TODO: add test here
-    expect(true).to eq(true)
+    d = FakeData.new
+    fd_adapter = FakeDataAdapter.new(d)
+    expect(fd_adapter.random_test_name.class).to eq(String)
+    expect(fd_adapter.random_test_name).to include(" ")
+    # recall that the name is random
+    expect(fd_adapter.random_test_name).not_to eq(fd_adapter.random_test_name)
   end
 
   it "has an adaptee which functions" do
-    # TODO: add test here
-    expect(true).to eq(true)
+    fd = FakeData.new
+    expect(fd.name.class).to eq(String)
+    expect(fd.name).to include(" ")
+    # recall that the name is random
+    expect(fd.name).not_to eq(fd.name)
   end
 
   it "has a target which functions" do
-    # TODO: add test here
-    expect(true).to eq(true)
+    f_data = FakeData.new
+    f_data_adapter = FakeDataAdapter.new(f_data)
+    td = TestData.new(f_data_adapter)
+    td.random_test_name
+    expect(td.random_test_name.class).to eq(String)
+    expect(td.random_test_name).to include(" ")
+    # recall that the name is random
+    expect(td.random_test_name).not_to eq(td.random_test_name)
   end
 
   it "is a client which utilizes the adapter pattern" do
-    # TODO: add test here
-    expect(true).to eq(true)
+    b = SectionBAdapterPattern.new
+    expect(b.use_our_target_with_adapter.class).to eq(String)
+    expect(b.use_our_target_with_adapter).to include(" ")
+    # recall that the name is random
+    expect(b.use_our_target_with_adapter).not_to eq(b.use_our_target_with_adapter)
   end
 end
